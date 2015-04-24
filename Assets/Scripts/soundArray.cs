cf4b19e27391f9c81f3b6034ab1748077469ce4a
pianovox <contact@doghousenyc.com>
2015-04-23 20:07:58 -0400

    cleaned up. but button text not working

--- Assets/Scripts/soundArray.cs
+++ Assets/Scripts/soundArray.cs
@@ -8,39 +8,33 @@ public class soundArray : MonoBehaviour
 	public AudioClip clip;
 	public int ButtonID;
 	public static int BankID = 0;
-
-
+	public Text bankNumberText;
+	public Text buttonText;
 	string audioPath;
-	//	HELP! Trying to connect to the inspector so I can use isInteractable or 
-	//	change color if it's disabled
-	//	public Button myButton; 
-	
+
 	void Start ()
-	{		
+	{	
 		if (clip == null) {
-
-			//	HELP! I know I'm reusing this line over and over. 
-			//	I can't initialize it up top because of ButtonID
-
 			audioPath = "Audio/" + (1000 + (BankID * 10) + ButtonID + 1);
 			clip = (AudioClip)Resources.Load (audioPath); 
 			print (audioPath);
 		}
 	}
+
+	void Update ()
+	{
+		bankNumberText.text = "" + (1000 + (BankID * 10) + 1) + " -" + (1000 + (BankID * 10) + 10);
+		buttonText.text = "" + (1000 + (BankID * 10) + ButtonID);
+	}
 	
-	public void Click () {
+	public void Click ()
+	{
 		if (clip == null) {
 			audioPath = "Audio/" + (1000 + (BankID * 10) + ButtonID + 1);
 			clip = (AudioClip)Resources.Load (audioPath); 
 			print (audioPath);
 
-//			if (!myButton.IsInteractable ()) {
-//				Debug.Log("Button has been Disabled");
-//			}
-
 		} else {
-			//	This is leftover. Disregard.
-			//	if (Input.GetMouseButtonDown(0)) {
 
 			audioPath = "Audio/" + (1000 + (BankID * 10) + ButtonID + 1);
 			clip = (AudioClip)Resources.Load (audioPath); 
@@ -49,22 +43,26 @@ public class soundArray : MonoBehaviour
 		}
 	}
 
-	public void BankUp (){
+	public void BankUp ()
+	{
 		BankID += 1;
 		print (BankID);
 	}
 
-	public void BankDown (){
+	public void BankDown ()
+	{
 		BankID -= 1;
 		print (BankID);
 	}
 
-	public void BankUpX10 (){
+	public void BankUpX10 ()
+	{
 		BankID += 10;
 		print (BankID);
 	}
 	
-	public void BankDownX10 (){
+	public void BankDownX10 ()
+	{
 		BankID -= 10;
 		print (BankID);
 	}
