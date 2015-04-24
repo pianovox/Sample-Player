dbbc557a032f41014e8fb32650df4fd4a4ee5e83
pianovox <contact@doghousenyc.com>
2015-04-24 12:15:58 -0400

    cleanup

--- Assets/Scripts/soundArray.cs
+++ Assets/Scripts/soundArray.cs
@@ -4,13 +4,14 @@ using UnityEngine.UI;
 
 public class soundArray : MonoBehaviour
 {
-	public static AudioClip[] list;
-	public AudioClip clip;
+	AudioClip clip;
+	AudioSource aSource;
 	public int ButtonID;
 	public static int BankID = 0;
 	public Text bankNumberText;
 	public Text buttonText;
 	string audioPath;
+	
 
 	void Start ()
 	{	
@@ -23,9 +24,17 @@ public class soundArray : MonoBehaviour
 
 	void Update ()
 	{
-		bankNumberText.text = "" + (1000 + (BankID * 10) + 1) + " -" + (1000 + (BankID * 10) + 10);
-		buttonText.text = "" + (1000 + (BankID * 10) + ButtonID);
+		string lowBank = "" + (1000 + (BankID * 10) + 1);
+		string highBank = ""+ (1000 + (BankID * 10) + 10);
+
+		bankNumberText.text = "Bank: " + BankID +"\n" + lowBank + " - " + highBank;
+
+
+
+
 	}
+
+
 	
 	public void Click ()
 	{
@@ -38,10 +47,29 @@ public class soundArray : MonoBehaviour
 
 			audioPath = "Audio/" + (1000 + (BankID * 10) + ButtonID + 1);
 			clip = (AudioClip)Resources.Load (audioPath); 
-			AudioSource.PlayClipAtPoint (clip, new Vector3 (5, 1, 2));
-			print (ButtonID);	
+//			AudioSource.PlayClipAtPoint (clip, new Vector3 (5, 1, 2));
+			print (ButtonID);
+
+			PlayClipAt(clip, transform.position);
 		}
 	}
+
+	AudioSource PlayClipAt(AudioClip clip, Vector3 pos)
+	{
+		GameObject tempGO = new GameObject("TempAudio"); 			// create the temp object
+		tempGO.transform.position = pos;			 				// set its position
+		aSource = tempGO.AddComponent<AudioSource>();	// add an audio source
+		aSource.clip = clip; 	 									// define the clip
+		
+		// set other aSource properties here, if desired
+		//================================================
+		aSource.Play(); 											// start the sound
+		Destroy(tempGO, clip.length); 								// destroy object after clip duration
+		return aSource; 											// return the AudioSource reference
+	
+	}
+
+
 
 	public void BankUp ()
 	{
