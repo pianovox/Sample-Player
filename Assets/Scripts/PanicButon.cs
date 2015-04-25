e76d8ab611a0ca61645549fbc03e1168d6293485
pianovox <contact@doghousenyc.com>
2015-04-25 16:37:47 -0400

    panic button works

--- Assets/Scripts/PanicButon.cs
+++ Assets/Scripts/PanicButon.cs
@@ -1,21 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 public class PanicButon : MonoBehaviour {
+
+	private GameObject TurnMeOff;
 	
 	public void Panic()
 	{
-		// destroy all tempAudio GameObjects
+		TurnMeOff = GameObject.Find ("TempAudio");
+		Destroy (TurnMeOff);
+
 	}
-	
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
